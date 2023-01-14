9939968914e0c49297983e4c2208cd14e19329db
ronak <ronakbisht475@gmail.com>
2023-01-14 20:59:47 +0530

    cahnge

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Router=require('./auth.js')
 const mongoose=require('mongoose')
 const cookies=require('cookie-parser')
 const DB="mongodb+srv://root:babyboss@cluster0.5mes4.mongodb.net/youtube?retryWrites=true&w=majority"
+mongoose.set('strictQuery',false)
 mongoose.connect(DB).then(()=>{
     console.log('connection Succesfull')
 }).catch((err)=>{
